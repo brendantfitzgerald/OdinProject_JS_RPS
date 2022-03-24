2a75a9ba85ef3047d12afa66d82fc9b0b7761800
FitzⓋ <git@yesfitz.com>
2022-03-24 14:26:52 -0500

    Replaced hardcoded strings with "options" array
    and added comments to functions

--- RPS.js
+++ RPS.js
@@ -1,16 +1,21 @@
-console.log("Hello world")
+console.log("Let's play Rock, Paper, Scissors!")
 
+//Declare viable options.
+const options = ['ROCK','PAPER','SCISSORS']
+
+//Generate Computer's Play
 function computerPlay(){
     switch(Math.floor(Math.random() * 3)){
         case 0:
-            return "ROCK"
+            return options[0]
         case 1:
-            return "PAPER"
+            return options[1]
         case 2:
-            return "SCISSORS"
+            return options[2]
     }
 }
 
+//Prompt for Player's Play and force to Upper Case.
 function playerPlay(){
     var input = prompt("Rock, Paper, Scissors?")
     input = input.toUpperCase()
@@ -21,11 +26,12 @@ function playerPlay(){
     return input
 }
 
+//Compare Computer's Play to Player's Play and return result for Round.
 function playRound(computerSelection,playerSelection){
     if (playerSelection == computerSelection){
         return("Draw, idiot. You both chose " + playerSelection + ".")
     }
-    else if ((playerSelection == 'ROCK' && computerSelection == "PAPER") || (playerSelection == 'PAPER' && computerSelection == "SCISSORS") || (playerSelection == 'SCISSORS' && computerSelection == "ROCK")){
+    else if ((playerSelection == options[0] && computerSelection == options[1]) || (playerSelection == options[1] && computerSelection == options[2]) || (playerSelection == options[2] && computerSelection == options[0])){
         return("You lose. " + computerSelection + " beats " + playerSelection +".")
     }
     else {
@@ -33,6 +39,7 @@ function playRound(computerSelection,playerSelection){
     }
 }
 
+//Play best of 5 Rounds and keep score.
 function game(){
     var computerScore = 0
     var playerScore = 0
@@ -51,6 +58,6 @@ function game(){
     }
 }
 
-const options = ['ROCK','PAPER','SCISSORS']
+
 
 game()
